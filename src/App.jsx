530b1b6d0f44c87bb372f4c35293c04d9496c2a5
Oscar Donaldson <oscar@donaldson.se>
2024-04-11 10:56:52 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <h1>Todo App</h1>
+      <p className="read-the-docs">
+        Welcome to a very very immaculate todo app
+      </p>
+    </>
+  )
+}
+
+export default App
