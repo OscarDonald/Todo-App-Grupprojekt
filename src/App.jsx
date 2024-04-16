5866ed5498174e48d086f2ac284dc9309aea4e9e
Nyat Ghirmazion <nyat.ghirmazion@chasacademy.se>
2024-04-16 15:21:28 +0200

    css and rendering tasks in column

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,13 @@
-import './App.css'
-import Header from './components/Header/Header'
-import CreateTask from './components/createTask/CreateTask'
-import Columns from './components/Columns/Columns.jsx'
+import './App.css';
+import CreateTask from './components/createTask/CreateTask';
+import Columns from './components/Columns/Columns.jsx';
 
 function App() {
 
 
   return (
     <main>
-      <Header />
       <CreateTask />
-      <Columns />
-      <Columns />
       <Columns />
     </main>
 
