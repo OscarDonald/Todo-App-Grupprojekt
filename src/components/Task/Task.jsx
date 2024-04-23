161632748e8d883b4bb1c8a28196950ca536c680
David Heidari <davidheidari2@gmail.com>
2024-04-23 15:22:37 +0200

    add drag and drop function

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -3,7 +3,13 @@ import styles from './Task.module.css'
 // renders a task
 export default function Task({task}) {
     return (
-        <div className={`${styles.task__card}`}>
+        <div
+            // Drag and Drop-function
+            draggable
+            onDragStart={(e) => { e.dataTransfer.setData('id', task.id) }}
+            //
+            
+            className={`${styles.task__card}`}>
             <h4>{task.title}</h4>
             <p>{task.description}</p>
             <p>Ska göras: {task.doDate}</p>
