0272a9d59eee935d4186f0cb41bf64be505722aa
Nyat Ghirmazion <nyat.ghirmazion@chasacademy.se>
2024-04-26 12:28:19 +0200

    alert on delete list

--- src/components/ColumnSettings/ColumnSettings.jsx
+++ src/components/ColumnSettings/ColumnSettings.jsx
@@ -45,7 +45,7 @@ export default function ColumnSettings({ column }) {
       </button>
       {isOpen && (
         <ul className={styles.settings__options}>
-          <li onClick={() => dispatch(removeColumn(column.id))}>Delete list</li>
+          <li onClick={confirmDelete}>Delete list</li>
           <li onClick={() => dispatch(moveColumn({ id: column.id, direction: 'left' }))}>Move left</li>
           <li onClick={() => dispatch(moveColumn({ id: column.id, direction: 'right' }))}>Move right</li>
           <li onClick={() => dispatch(copyColumn(column.id))}>Copy list</li>
